4ba0da4d26fac4243f8f3b7f932e2cec8b80823c
BiroKrisz <biro.s.kr@gmail.com>
2022-05-11 18:15:12 +0200

    Added tasks

--- Vue/vue-task/src/App.vue
+++ Vue/vue-task/src/App.vue
@@ -1,30 +1,75 @@
 <template>
-<div class="container">
-  <Header title="Task Tracker"/>
-</div>
-
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" @toggle-reminder="toggleReminder" :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header
-  }
-}
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "Feb 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "Feb 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Shopping with Anna",
+        day: "Marc 6th at 2:30pm",
+        reminder: true,
+      },
+      {
+        text: "Take Hostage",
+        day: "Today",
+        reminder: false,
+        id: 5,
+      },
+    ];
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
